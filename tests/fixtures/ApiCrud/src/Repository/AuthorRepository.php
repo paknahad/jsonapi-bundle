7a5f64134eb52a0b4e9573e431f6030f45586df4
Alex Lisenkov <alex@create.nl>
2020-02-05 18:06:13 +0100

    Add symfony 5 support

--- tests/fixtures/ApiCrud/src/Repository/AuthorRepository.php
+++ tests/fixtures/ApiCrud/src/Repository/AuthorRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Author;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Author|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class AuthorRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Author::class);
     }
