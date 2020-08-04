18860d9f06bbacddd71b1377ca02f9a2ab25fd0c
Marko Pađen <markopaden3@gmail.com>
2020-08-04 18:35:26 +0200

    add interface instead of list

--- src/Resources/skeleton/api/controller/Controller.tpl.php
+++ src/Resources/skeleton/api/controller/Controller.tpl.php
@@ -47,7 +47,6 @@ class <?= $class_name ?> extends Controller
 
         $<?= $entity_var_name ?> = $this->jsonApi()->hydrate(new <?= $create_hydrator_class_name ?>($entityManager, $exceptionFactory), new <?= $entity_class_name ?>());
 
-        /** @var ConstraintViolationList $errors */
         $errors = $validator->validate($<?= $entity_var_name ?>);
         if ($errors->count() > 0) {
             return $this->validationErrorResponse($errors);
