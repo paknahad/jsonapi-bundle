5825ce9ecfb38c7e1837bbe4c63d528254015e25
Marko <markopaden3@gmail.com>
2020-08-12 10:13:45 +0200

    Merge pull request #73 from mpaden3/fix/document
    Move duplicated code to abstract Document classes

---
+++ src/Document/AbstractCollectionDocument.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Paknahad\JsonApiBundle\Document;
+
+use WoohooLabs\Yin\JsonApi\Schema\Document\AbstractCollectionDocument as BaseDocument;
+use WoohooLabs\Yin\JsonApi\Schema\JsonApiObject;
+
+abstract class AbstractCollectionDocument extends BaseDocument
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getJsonApi(): ?JsonApiObject
+    {
+        return new JsonApiObject('1.0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMeta(): array
+    {
+        return [];
+    }
+}
